9759c2c1ceda743e1b2c6a58426aa91169dbc6a7
MichelleChen2331 <mchen61@cslab1-7>
2024-03-13 14:47:00 -0400

    create file

---
+++ t00.py
@@ -0,0 +1,22 @@
+"""
+
+MC Question:
+Which of the following 
+Options:
+A)
+B)
+C)
+D)
+
+Answer:
+
+
+===============================
+Programming question:
+
+
+Write a function that counts how many vowels are in a word.
+
+
+Possible solution:
+"""
