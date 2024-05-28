c1338e38769be4dd323723282adce5fc9e67c8cd
MichelleChen2331 <mchen61@cslab1-7>
2024-05-28 14:48:34 -0400

    update

--- py/w38_rand.py
+++ py/w38_rand.py
@@ -3,4 +3,22 @@ print('Content-type: text/html\n')
 
 from random import random
 
-print('Some random number:', random())
+def html():
+    stuff = '''<!DOCTYPE html>
+
+<html lang="en">
+
+  <head>
+    <meta charset="utf-8">
+    <title>Lottery Number</title>
+  </head>
+
+  <body>
+    <p>
+      Reload to generate a random number
+    </p>
+  </body>
+
+</html>'''
+    print(stuff)
+print('Some random number:', html, random())
